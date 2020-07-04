c263dffb9109e7a656ba4c16f90d5dff87a1192f
rickshev <rick.shevlin@gmail.com>
2020-07-04 19:33:44 -0400

    Working bar charts in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,131 @@
+function plotAges() {
+    d3.json("http://localhost:5000/ages").then(function (data) {
+        var age = data.map(d => d.age);
+        var count = data.map(d => d.count);
 
-d3.json("http://localhost:5000/data").then(function (data) {
-    var state = data.map(d => d.state);
-    var age = data.map(d => d.age);
-    var gender = data.map(d => d.gender);
-    var race = data.map(d => d.race);
-    var mental_illness = data.map(d => d.illness);
-    var year = data.map(d => d.year);
+        var trace = {
+            type: "bar",
+            x: age,
+            y: count
+        }
 
-    var trace1 = {
-        type: "bar",
-        x: gender,
-        y: data.length
-    };
+        var data = [trace];
 
-    var data1 = [trace1];
+        // var layout = {
+        //     title: "",
 
-    // var layout = {
-    //     title: "",
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+// plotAges();
 
-    // }
+function plotStates() {
+    d3.json("http://localhost:5000/states").then(function (data) {
+        var state = data.map(d => d.state);
+        var count = data.map(d => d.count);
 
-    Plotly.newPlot("bar", data1);
+        var trace = {
+            type: "bar",
+            x: state,
+            y: count
+        }
 
-})
+        var data = [trace];
 
+        // var layout = {
+        //     title: "",
 
-// function buildPlot() {
-//     d3.json("http://localhost:5000/data").then(function (data) {
-//         var state = data.map(d => d.state);
-//         var age = data.map(d => d.age);
-//         var gender = data.map(d => d.gender);
-//         var race = data.map(d => d.race);
-//         var mental_illness = data.map(d => d.illness);
-//         var year = data.map(d => d.year);
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+// plotStates();
 
-//         getData();
+function plotYears() {
+    d3.json("http://localhost:5000/years").then(function (data) {
+        var year = data.map(d => d.year);
+        var count = data.map(d => d.count);
 
-        
-//     })
-// }
+        var trace = {
+            type: "bar",
+            x: year,
+            y: count
+        }
+
+        var data = [trace];
+
+        // var layout = {
+        //     title: "",
+
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+// plotYears();
+
+function plotIllness() {
+    d3.json("http://localhost:5000/illness").then(function (data) {
+        var illness = data.map(d => d.mental_illness);
+        var count = data.map(d => d.count);
+
+        var trace = {
+            type: "bar",
+            x: illness,
+            y: count
+        }
+
+        var data = [trace];
+
+        // var layout = {
+        //     title: "",
+
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+// plotIllness();
+
+function plotGenders() {
+    d3.json("http://localhost:5000/gender").then(function (data) {
+        var gender = data.map(d => d.gender);
+        var count = data.map(d => d.count);
+
+        var trace = {
+            type: "bar",
+            x: gender,
+            y: count
+        }
+
+        var data = [trace];
+
+        // var layout = {
+        //     title: "",
+
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+// plotGenders();
+
+function plotCause() {
+    d3.json("http://localhost:5000/causeOfDeath").then(function (data) {
+        var cause = data.map(d => d.cause);
+        var count = data.map(d => d.count);
+
+        var trace = {
+            type: "bar",
+            x: cause,
+            y: count
+        }
+
+        var data = [trace];
+
+        // var layout = {
+        //     title: "",
+
+        // }
+        Plotly.newPlot("bar", data);
+    })
+}
+plotCause();
