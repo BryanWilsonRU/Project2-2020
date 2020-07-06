dde3ff42c164352cf062844ac3f38ee1e01783d8
rickshev <rick.shevlin@gmail.com>
2020-07-05 21:33:46 -0400

    Modified bar plots to get working interaction

--- static/js/plots.js
+++ static/js/plots.js
@@ -20,116 +20,173 @@ function init() {
 }
 // plotAges();
 
-function updatePlotly(){
-    
-}
+d3.selectAll("#selDataset").on("change", updatePlotly);
 
-function plotStates() {
-    d3.json("/states").then(function (data) {
-        var state = data.map(d => d.state);
-        var count = data.map(d => d.count);
+function updatePlotly() {
+    var dropdownMenu = d3.select("#selDataset");
+    var dataset = dropdownMenu.property("value");
 
-        var trace = {
-            type: "bar",
-            x: state,
-            y: count
-        }
+    var x = [];
+    var y = [];
 
-        var data = [trace];
+    if (dataset === "dataset1") {
+        d3.json("/states").then(function (data) {
+            x = data.map(d => d.state);
+            y = data.map(d => d.count);
 
-        // var layout = {
-        //     title: "",
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        })
+    }
+    if (dataset === "dataset2") {
+        d3.json("/years").then(function (data) {
+            x = data.map(d => d.year);
+            y = data.map(d => d.count);
 
-        // }
-        Plotly.newPlot("bar", data);
-    })
-}
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        })
+    }
+    if (dataset === "dataset3") {
+        d3.json("/illness").then(function (data) {
+            x = data.map(d => d.mental_illness);
+            y = data.map(d => d.count);
+
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        })
+    }
+    if (dataset === "dataset4") {
+        d3.json("/gender").then(function (data) {
+            x = data.map(d => d.gender);
+            y = data.map(d => d.count);
+
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        })
+    }
+    if (dataset === "dataset5") {
+        d3.json("/causeDeath").then(function (data) {
+            x = data.map(d => d.cause);
+            y = data.map(d => d.count);
+
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        })
+    }
+    if (dataset === "dataset6"){
+        return init();
+    }
+    // Plotly.restyle("bar", "x", [x]);
+    // Plotly.restyle("bar", "y", [y]);
+};
+
+// function plotStates() {
+//     d3.json("/states").then(function (data) {
+//         var state = data.map(d => d.state);
+//         var count = data.map(d => d.count);
+
+//         var trace = {
+//             type: "bar",
+//             x: state,
+//             y: count
+//         }
+
+//         var data = [trace];
+
+//         // var layout = {
+//         //     title: "",
+
+//         // }
+//         Plotly.newPlot("bar", data);
+//     })
+// }
 // plotStates();
 
-function plotYears() {
-    d3.json("/years").then(function (data) {
-        var year = data.map(d => d.year);
-        var count = data.map(d => d.count);
+// function plotYears() {
+//     d3.json("/years").then(function (data) {
+//         var year = data.map(d => d.year);
+//         var count = data.map(d => d.count);
 
-        var trace = {
-            type: "bar",
-            x: year,
-            y: count
-        }
+//         var trace = {
+//             type: "bar",
+//             x: year,
+//             y: count
+//         }
 
-        var data = [trace];
+//         var data = [trace];
 
-        // var layout = {
-        //     title: "",
+//         // var layout = {
+//         //     title: "",
 
-        // }
-        Plotly.newPlot("bar", data);
-    })
-}
+//         // }
+//         Plotly.newPlot("bar", data);
+//     })
+// }
 // plotYears();
 
-function plotIllness() {
-    d3.json("/illness").then(function (data) {
-        var illness = data.map(d => d.mental_illness);
-        var count = data.map(d => d.count);
+// function plotIllness() {
+//     d3.json("/illness").then(function (data) {
+//         var illness = data.map(d => d.mental_illness);
+//         var count = data.map(d => d.count);
 
-        var trace = {
-            type: "bar",
-            x: illness,
-            y: count
-        }
+//         var trace = {
+//             type: "bar",
+//             x: illness,
+//             y: count
+//         }
 
-        var data = [trace];
+//         var data = [trace];
 
-        // var layout = {
-        //     title: "",
+//         // var layout = {
+//         //     title: "",
 
-        // }
-        Plotly.newPlot("bar", data);
-    })
-}
+//         // }
+//         Plotly.newPlot("bar", data);
+//     })
+// }
 // plotIllness();
 
-function plotGenders() {
-    d3.json("/gender").then(function (data) {
-        var gender = data.map(d => d.gender);
-        var count = data.map(d => d.count);
+// function plotGenders() {
+//     d3.json("/gender").then(function (data) {
+//         var gender = data.map(d => d.gender);
+//         var count = data.map(d => d.count);
 
-        var trace = {
-            type: "bar",
-            x: gender,
-            y: count
-        }
+//         var trace = {
+//             type: "bar",
+//             x: gender,
+//             y: count
+//         }
 
-        var data = [trace];
+//         var data = [trace];
 
-        // var layout = {
-        //     title: "",
+//         // var layout = {
+//         //     title: "",
 
-        // }
-        Plotly.newPlot("bar", data);
-    })
-}
+//         // }
+//         Plotly.newPlot("bar", data);
+//     })
+// }
 // plotGenders();
 
-function plotCause() {
-    d3.json("/causeDeath").then(function (data) {
-        var cause = data.map(d => d.cause);
-        var count = data.map(d => d.count);
+// function plotCause() {
+//     d3.json("/causeDeath").then(function (data) {
+//         var cause = data.map(d => d.cause);
+//         var count = data.map(d => d.count);
 
-        var trace = {
-            type: "bar",
-            x: cause,
-            y: count
-        }
+//         var trace = {
+//             type: "bar",
+//             x: cause,
+//             y: count
+//         }
 
-        var data = [trace];
+//         var data = [trace];
 
-        // var layout = {
-        //     title: "",
+//         // var layout = {
+//         //     title: "",
 
-        // }
-        Plotly.newPlot("bar", data);
-    })
-}
-plotCause();
+//         // }
+//         Plotly.newPlot("bar", data);
+//     })
+// }
+init();
