7fab2537b788ceebe0630c45c3d01551438563a5
rickshev <rick.shevlin@gmail.com>
2020-07-09 16:27:11 -0400

    Prepping for Heroku deployment

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -5,11 +5,15 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 from flask import render_template
+import os
 
 #################################################
 # Database Setup
 #################################################
-engine = engine = create_engine('postgresql://postgres:postgres@localhost:5432/police_violence_db')
+app = Flask(__name__)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/police_violence_db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgres://fyrmxogkqhwula:f86d52d7458b3178d2b78f6b47b5d0d4b512efdf4025f8dbb2a55cccdd578539@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d797ubnppbaeh8') or 'postgresql://postgres:postgres@localhost:5432/police_violence_db'
 
 # reflect an existing database into a new model
 Base = automap_base()
